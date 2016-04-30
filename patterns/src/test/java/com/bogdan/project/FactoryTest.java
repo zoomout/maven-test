6b84415da2cb2afa26ceab8ad01036bca1d8a29a
zoomout <zoomout@ukr.net>
2016-04-30 23:42:50 +0200

    added factory pattern

---
+++ patterns/src/test/java/com/bogdan/project/FactoryTest.java
@@ -0,0 +1,29 @@
+package com.bogdan.project;
+
+import com.bogdan.project.factory.ShapeFactory;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Created by zoomout on 4/30/16.
+ */
+public class FactoryTest {
+    ShapeFactory shapeFactory = new ShapeFactory();
+
+    @Test
+    public void drawCircle() {
+        assertEquals(shapeFactory.getShape("CIRCLE").draw(), "circle");
+
+    }
+
+    @Test
+    public void drawRectangle() {
+        assertEquals(shapeFactory.getShape("RECTANGLE").draw(), "rectangle");
+    }
+
+    @Test
+    public void drawSquare() {
+        assertEquals(shapeFactory.getShape("SQUARE").draw(), "square");
+    }
+}
