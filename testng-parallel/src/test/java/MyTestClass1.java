e932fca721ac38f193197de8dc6a613514fdbac9
Bogdan Zastavnyi <bogdan.zastavnyi@irdeto.com>
2016-11-22 01:41:51 +0100

    shared testng field when running in parallel

--- testng-parallel/src/test/java/MyTestClass1.java
+++ testng-parallel/src/test/java/MyTestClass1.java
@@ -1,5 +1,8 @@
 import org.slf4j.LoggerFactory;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.Random;
 
 /**
  * Created by zoomout on 5/24/16.
@@ -7,15 +10,23 @@ import org.testng.annotations.Test;
 public class MyTestClass1 extends AbstractTest {
     private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(AbstractTest.class.getSimpleName());
 
+    private int num;
+
+    @BeforeMethod
+    public void beforeMethod() {
+        Random random = new Random();
+        num = random.nextInt(1000);
+    }
+
     @Test
     public void myTestMethod1_1() {
-        LOG.info("myTestMethod1_1");
+        LOG.info("myTestMethod1_1; sometimes_not_random_num=" + num);
         Sleep.sleep(500);
     }
 
     @Test
     public void myTestMethod1_2() {
-        LOG.info("myTestMethod1_2");
+        LOG.info("myTestMethod1_2; sometimes_not_random_num=" + num);
         Sleep.sleep(500);
     }
 }
--- testng-parallel/src/test/java/MyTestClass2.java
+++ testng-parallel/src/test/java/MyTestClass2.java
@@ -1,5 +1,9 @@
 import org.slf4j.LoggerFactory;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.lang.reflect.Method;
+import java.util.*;
 
 /**
  * Created by zoomout on 5/24/16.
@@ -7,15 +11,23 @@ import org.testng.annotations.Test;
 public class MyTestClass2 extends AbstractTest {
     private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(AbstractTest.class.getSimpleName());
 
+    private Map<String, Integer> map = new HashMap<>();
+
+    @BeforeMethod
+    public void beforeMethod(Method m) {
+        Random random = new Random();
+        map.put(m.getName(), random.nextInt(1000));
+    }
+
     @Test
-    public void myTestMethod2_1() {
-        LOG.info("myTestMethod2_1");
+    public void myTestMethod2_1(Method m) {
+        LOG.info("myTestMethod2_1; random_num=" + map.get(m.getName()));
         Sleep.sleep(500);
     }
 
     @Test
-    public void myTestMethod2_2() {
-        LOG.info("myTestMethod2_2");
+    public void myTestMethod2_2(Method m) {
+        LOG.info("myTestMethod2_2; random_num=" + map.get(m.getName()));
         Sleep.sleep(500);
     }
 }
